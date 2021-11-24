ecc6fd79f9c34d91f71cae5c3e1031a73a053cf6
Tejas Tendulkar <37808159+tejasten5@users.noreply.github.com>
2021-11-24 20:25:31 +0530

    Update reverse_arr.py

--- reverse_arr.py
+++ reverse_arr.py
@@ -1,3 +1,4 @@
+# while loop
 def reverse_arr(arr):
     start,end = 0,len(arr)-1
     
@@ -11,3 +12,15 @@ def reverse_arr(arr):
 
 result_arr = reverse_arr([50,49,48,47,46,45,44,43])
 print(result_arr)
+
+# for loop
+def reverseArray(arr):
+    j = len(arr)-1
+    for i in range(len(arr)):
+        if i < j:
+            arr[i],arr[j-1] = arr[j-1],arr[i] 
+        j -= 1
+    print(arr)
+
+arr = [65,64,63,62,61,60]
+reverseArray(arr)
