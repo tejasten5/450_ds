6be408b2482d0af488872827946bd23911cfe879
Tejas Tendulkar <37808159+tejasten5@users.noreply.github.com>
2021-11-24 21:14:25 +0530

    Day-2
    Day-2

--- shiftNegative.py
+++ shiftNegative.py
@@ -1,1 +1,11 @@
 
+def shiftNegative(arr):
+    j = 0
+    for i in range(len(arr)):
+        if (arr[i] < 0):
+            arr[i],arr[j] = arr[j],arr[i]
+            j += 1
+    print(arr)
+    
+arr = [-3,-1, 2,  4, 5, 6, -7, 8, 9]
+shiftNegative(arr)
