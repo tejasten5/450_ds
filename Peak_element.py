e3c495d8adccf86e54affa6ae0583eba5d4c5b9a
Tejas Tendulkar <37808159+tejasten5@users.noreply.github.com>
2021-11-25 21:03:32 +0530

    Day-3

--- Peak_element.py
+++ Peak_element.py
@@ -1,1 +1,13 @@
 
+class Solution:   
+    def peakElement(self,arr, n):
+        # Code here
+        max_ele = 0
+        for i in range(n):
+            
+            if arr[i] > max_ele:
+                max_ele = arr[i]
+                idx = i
+            
+               
+        return idx
